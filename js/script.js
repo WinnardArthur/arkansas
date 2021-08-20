dbd1887b4546c767737a8f5239f97b9eb40587e3
JadenXmith <jadenxmith@gmail.com>
2021-08-20 22:52:57 +0000

    Created home.html page

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$('.selectFood__carousel').owlCarousel({
+    loop: false,
+    nav: false,
+    dots: false,
+    autoplay: 0,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 1000,
+    mouseDrag: false,
+    responsive: {
+        0: {items: 1},
+        600: {items: 1},
+        1000: { items: 1}
+    }
+})
+
+const testimonialOwl = $('.selectFood__carousel')
+testimonialOwl.owlCarousel();
+
+$('.nextFood').click(function(){
+    testimonialOwl.trigger('next.owl.carousel');
+})
+$('.prevFood').click(function(){
+    testimonialOwl.trigger('prev.owl.carousel', [300])
+})
