6922c5224165111677e531f9fc388374d63cf913
JadenXmith <jadenxmith@gmail.com>
2021-08-21 18:16:33 +0000

    Created LapSystem.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+$('.heroCarousel').owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: 5000,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 1000,
+    mouseDrag: false,
+    responsive: {
+        0: {items: 1},
+        600: {items: 1},
+        1000: { items: 1}
+    }
+})
+
+const heroOwl = $('.heroCarousel')
+heroOwl.owlCarousel();
+
+
+
+$('.nextHero').click(function(){
+    heroOwl.trigger('next.owl.carousel');
+})
+$('.prevHero').click(function(){
+    heroOwl.trigger('prev.owl.carousel', [300])
+})
+
 $('.selectFood__carousel').owlCarousel({
     loop: false,
     nav: false,
